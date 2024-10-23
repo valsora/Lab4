81e2adb22316e8a07a9423a0a6c70d8dd07644ef
valsora <valsora9@gmail.com>
2024-10-23 17:27:22 +0300

    3

---
+++ CopyFileContent.java
@@ -0,0 +1,22 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CopyFileContent {
+    public static void main(String[] args) {
+        try {
+            FileReader fileReader = new FileReader("from.txt");
+            FileWriter fileWriter = new FileWriter("to.txt");
+            int i = fileReader.read();
+            while (i != -1) {
+                fileWriter.write(i);
+                i = fileReader.read();
+            }
+            fileReader.close();
+            fileWriter.close();
+
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+    }
+}
