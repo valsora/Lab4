f3b53b5dded689476d9784e25d0191cfcf59d902
valsora <valsora9@gmail.com>
2024-10-23 16:50:08 +0300

    initial

---
+++ ArrayAverage.java
@@ -0,0 +1,16 @@
+public class ArrayAverage {
+    public static void main(String[] args) {
+            int[] arr = new int[] {1, 2, 3, 4, 5};
+            int sum = 0;
+            try {
+                for (int i = 0; i < arr.length; i++) {
+                    sum += arr[i];
+                }
+                System.out.println(sum / arr.length);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(e.toString());
+            } catch (ArithmeticException e) {
+                System.out.println(e.toString());
+            }
+    }
+}
